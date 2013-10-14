4042a5c7929ffec7bd6a4c62117bc04c35c4559c
mouyigang <mouyigang@gmail.com>
2013-10-14 22:26:33 +0800

    new user support

---
+++ src/main/java/com/check/v3/web/controller/UserController.java
@@ -0,0 +1,40 @@
+package com.check.v3.web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.check.v3.domain.User;
+import com.check.v3.service.UserService;
+
+
+@Controller
+public class UserController {
+
+	@Autowired
+	UserService userService;
+
+	@RequestMapping(value="/users/new",method=RequestMethod.GET)
+	public String newUser(Model model)
+	{
+		model.addAttribute("user", new User());
+		return "users/new";
+	}
+	@RequestMapping(value="/users",method = RequestMethod.POST)
+	public String create(
+			@ModelAttribute User user,
+			Model model) {
+//		User user = new User();
+//		user.setName(username);
+//		user.setPassword(password);
+//		user.setAccount(account);
+		System.out.println(user.getName().toString());
+//		user.setPassword(user.getPassword());
+		userService.save(user);
+		return "redirect:users/show";
+	}
+
+}
