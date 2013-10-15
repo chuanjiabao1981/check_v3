d5ef0abd9932724dfcd87f53b338b5c2ad3c9f57
mouyigang <mouyigang@gmail.com>
2013-10-15 22:53:04 +0800

    show user support

--- src/main/java/com/check/v3/web/controller/UserController.java
+++ src/main/java/com/check/v3/web/controller/UserController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -29,7 +30,12 @@ public class UserController {
 			Model model) {
 		System.out.println(user.getName().toString());
 		userService.save(user);
-		return "redirect:users/show";
+		return "redirect:/users/" + user.getId();
 	}
-
+	@RequestMapping(value = "/users/{id}", method = RequestMethod.GET)
+    public String show(@PathVariable("id") Long id, Model uiModel) {
+        User user = userService.findById(id);
+		uiModel.addAttribute("user", user);
+        return "users/show";
+    }		
 }
